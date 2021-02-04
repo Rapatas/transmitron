40be0c9f40673fa1b643dcfecbea0a6f80b96706
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2021-02-04 20:45:59 +0200

    Fix undefined json extractor

--- src/Helpers/Helpers.hpp
+++ src/Helpers/Helpers.hpp
@@ -12,6 +12,52 @@ static std::optional<T> extract(
   const std::string &key
 );
 
+template<>
+std::optional<unsigned> extract<unsigned>(
+  const nlohmann::json &data,
+  const std::string &key
+) {
+  auto it = data.find(key);
+  if (
+    it == std::end(data)
+    || it->type() != nlohmann::json::value_t::number_unsigned
+  ) {
+    return std::nullopt;
+  }
+  return it->get<unsigned>();
+}
+
+template<>
+std::optional<std::string> extract<std::string>(
+  const nlohmann::json &data,
+  const std::string &key
+) {
+  auto it = data.find(key);
+  if (
+    it == std::end(data)
+    || it->type() != nlohmann::json::value_t::string
+  ) {
+    return std::nullopt;
+  }
+  return it->get<std::string>();
+}
+
+template<>
+std::optional<bool> extract<bool>(
+  const nlohmann::json &data,
+  const std::string &key
+) {
+  auto it = data.find(key);
+  if (
+    it == std::end(data)
+    || it->type() != nlohmann::json::value_t::boolean
+  ) {
+    return std::nullopt;
+  }
+  return it->get<bool>();
+}
+
+
 }
 
 #endif // HELPERS_HELPERS_HPP
