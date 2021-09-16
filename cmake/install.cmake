a6b7766644c94fc9c0ff7356b4be2f2f211deabe
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2021-09-16 08:03:45 +0300

    feat(cmake): add linux icon and launcher

---
+++ cmake/install.cmake
@@ -0,0 +1,18 @@
+
+include(GNUInstallDirs)
+
+install(
+  FILES
+    ${CMAKE_SOURCE_DIR}/resources/images/transmitron.ico
+  DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}
+)
+
+
+install(
+  TARGETS 
+    ${PROJECT_NAME}
+  RUNTIME 
+  DESTINATION bin
+)
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/resources/desktop)
