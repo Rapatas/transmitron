06996cef4a4863b246a1b209e236b7ea62139fc7
Andy Rapatas <15367779+Rapatas@users.noreply.github.com>
2022-02-19 07:32:06 +0200

    refactor(extract): move implementation to source file

--- src/Common/Extract.hpp
+++ src/Common/Extract.hpp
@@ -8,7 +8,7 @@ namespace Common
 {
 
 template<typename T>
-static std::optional<T> extract(
+std::optional<T> extract(
   const nlohmann::json &data,
   const std::string &key
 );
@@ -17,46 +17,19 @@ template<>
 std::optional<unsigned> extract<unsigned>(
   const nlohmann::json &data,
   const std::string &key
-) {
-  auto it = data.find(key);
-  if (
-    it == std::end(data)
-    || it->type() != nlohmann::json::value_t::number_unsigned
-  ) {
-    return std::nullopt;
-  }
-  return it->get<unsigned>();
-}
+);
 
 template<>
 std::optional<std::string> extract<std::string>(
   const nlohmann::json &data,
   const std::string &key
-) {
-  auto it = data.find(key);
-  if (
-    it == std::end(data)
-    || it->type() != nlohmann::json::value_t::string
-  ) {
-    return std::nullopt;
-  }
-  return it->get<std::string>();
-}
+);
 
 template<>
 std::optional<bool> extract<bool>(
   const nlohmann::json &data,
   const std::string &key
-) {
-  auto it = data.find(key);
-  if (
-    it == std::end(data)
-    || it->type() != nlohmann::json::value_t::boolean
-  ) {
-    return std::nullopt;
-  }
-  return it->get<bool>();
-}
+);
 
 }
 
